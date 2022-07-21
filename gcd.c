020e43928c06588f226df51a01f93b94e450456a
koshal <koshalg0777@gmail.com>
2022-07-22 01:33:49 +0530

    gcd

---
+++ gcd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int gcd(int m,int n){
+    if(n==0){
+        return m;
+    }
+    else{
+        return(gcd(n,(m%n)));
+    }
+}
+int main(){
+    int n,m;
+    int GCD;
+    printf("Enter the values:");
+    scanf("%d %d",&m,&n);
+    GCD=gcd(m,n);
+    printf("The gcd of %d and %d is %d",m,n,GCD);
+}
